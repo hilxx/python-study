9bd3a8ba4b2eb2c11e410a87dd821c1eba18a831
이은정 <eunjung377@naver.com>
2026-02-04 15:55:18 +0900

    Add: level1 basic 연습 (출력, 변수, 입력, 연산자)

---
+++ exercises/1-basics/02-variable.py
@@ -0,0 +1,19 @@
+age = 20
+name = "Jenny"
+height = 180
+
+print(age)
+print(name)
+print(height)
+
+print(type(age)) #<class 'int'>
+print(type(name)) #<class 'str'>
+print(type(height)) #<class 'int'>
+
+# ✔ 타입을 쓰지 않는다
+# ✔ 값이 바뀔 수 있다
+
+# 정수(int)	10
+# 실수(float)	3.14
+# 문자열(str)	"hello"
+# 불리언(bool)	True, False
