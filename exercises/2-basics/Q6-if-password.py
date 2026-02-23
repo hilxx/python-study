622977a4b4ac76076e2f3fad5c0817a8aa6659cf
이은정 <eunjung377@naver.com>
2026-02-23 16:02:04 +0900

    [Add] 연습 문제6 - 비밀번호 검증 프로그램

---
+++ exercises/2-basics/Q6-if-password.py
@@ -0,0 +1,12 @@
+# 문제6 : 비밀번호 검증
+## 비밀번호 검증하는 프로그램을 작성하시오.
+
+password = "54321"
+
+user_password = input("비밀번호를 입력하세요 :")
+
+if user_password == password:
+  print("로그인 성공")
+else:
+  print("로그인 실패")
+
